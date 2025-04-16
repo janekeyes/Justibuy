44f174f95fbd1882723d2e025012ba6c98aa3729
janekeyes <janekeyes123@gmail.com>
2025-04-16 12:36:33 +0100

    save orb keypoint image for item upload

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,8 @@
+import cv2
+import numpy as np
+from PIL import Image
+from io import BytesIO
+from django.core.files.base import ContentFile
 from django.db import models
 
 class UserProfile(models.Model):
@@ -20,7 +25,26 @@ class Clothing(models.Model):
     description = models.CharField(max_length=255)
     size = models.CharField(max_length=50, null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='clothing_images/', blank=True, null=True)  # Ensure upload path is correct
+    image = models.ImageField(upload_to='clothing_images/', blank=True, null=True)  
+    image_keypoints = models.ImageField(upload_to='keypoints/', null=True, blank=True)
 
-    def __str__(self):
-        return self.name
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if self.image and not self.image_keypoints:
+            img_path = self.image.path
+            img_cv = cv2.imread(img_path)
+            gray = cv2.cvtColor(img_cv, cv2.COLOR_BGR2GRAY)
+            orb = cv2.ORB_create()
+            keypoints = orb.detect(gray, None)
+            keypoints, descriptors = orb.compute(gray, keypoints)
+
+            img_with_kp = cv2.drawKeypoints(img_cv, keypoints, None, color=(0,255,0), flags=0)
+
+            is_success, buffer = cv2.imencode(".jpg", img_with_kp)
+            if is_success:
+                io_buf = BytesIO(buffer)
+                self.image_keypoints.save(f'kp_{self.image.name}', ContentFile(io_buf.read()), save=False)
+
+        super().save(update_fields=['image_keypoints'])
+
